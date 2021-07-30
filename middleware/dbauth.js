d7d4e4adcf45dd9f875940eaf446267f181ab66d
Adis Jugo <jugo.adis1@gmail.com>
2021-07-31 01:17:46 +0200

    solve security issues / fix runtime problems

--- middleware/dbauth.js
+++ middleware/dbauth.js
@@ -2,27 +2,26 @@ const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../model/user');
-const bcrypt = require('bcrypt');
 const { registerValidation, loginValidation } = require('../validation');
 
 
 router.post('/login', async (req, res) => {
     const { error } = loginValidation(req.body);
 
-    if (error) { 
+    if (error) {
         return res.status(400).send(error.details[0].message);
     }
 
     // Check if user is registered
     const user = await User.findOne({ email: req.body.email });
 
-    if(!user) return res.status(400).send('Email or password is wrong');
+    if (!user) return res.status(400).send('Email or password is wrong');
 
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if(!validPassword) return res.status(400).send('Password is wrong');
+    if (!validPassword) return res.status(400).send('Password is wrong');
 
     // Create JWT token
-    const token = jwt.sign({ _id: user._id, iat: Math.floor(Date.now() / 1000) - 30 }, process.env.TOKEN_SECRET, {expiresIn: '1h'});
+    const token = jwt.sign({ _id: user._id, iat: Math.floor(Date.now() / 1000) - 30 }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
     res.header('auth-token', token).send(token);
 
     // res.send('Logged in');
@@ -33,14 +32,14 @@ router.post('/register', async (req, res) => {
     // Validate data
     const { error } = registerValidation(req.body);
 
-    if (error) { 
+    if (error) {
         return res.status(400).send(error.details[0].message);
     }
 
     // Check if user already exists
     const emailExists = await User.findOne({ email: req.body.email });
 
-    if(emailExists) return res.status(400).send('Email already registered');
+    if (emailExists) return res.status(400).send('Email already registered');
 
     // Hash passwords
     const salt = await bcrypt.genSalt(10);
